8c231a7b7b5c5b00c258948ee34e6c1542b3782c
Sebastien Lemieux <s.lemieux@umontreal.ca>
2026-02-21 13:08:14 -0500

    Added a test script

---
+++ scripts/test.jl
@@ -0,0 +1,25 @@
+include("../src/LincsProject.jl")
+using DataFrames
+
+l = LincsProject.Lincs("/scratch/lincs_beta/", "level3_beta_all_n3026460x12328.gctx", "data/lincs.jld2");
+trt_cp = l[:pert_type, :trt_cp]
+
+lcp = l[trt_cp]
+gdf = groupby(lcp, :cell_mfc_name)
+
+cp_count = combine(gdf, :pert_id => (x -> unique(x) |> length) => :nb_cp)
+z = sort(cp_count, :nb_cp; rev=true)
+cnv = z[1:10,:cell_mfc_name]
+
+i2s = Dict([row.pert_id => row.canonical_smiles for row in eachrow(l.compound)]);
+for a in eachindex(cnv)
+    pa = lcp[lcp.cell_mfc_name .== cnv[a], :pert_id] |> unique
+    sa = Set([i2s[p] for p ∈ pa])
+    for b in (a+1:length(cnv))
+        pb = lcp[lcp.cell_mfc_name .== cnv[b], :pert_id] |> unique
+        sb = Set([i2s[p] for p ∈ pb])
+        ci = sa ∩ sb
+        println("$(cnv[a]) $(cnv[b]) -> $(length(ci))")
+    end
+end
+
